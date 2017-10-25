79cda84db44cc1933c0d847d9fe85efcdbd12815
Ellen Wyllie <elw2257@lib-pclem030.lib.utexas.edu>
2017-10-25 15:07:53 -0500

    Add sign up and sign in pages

---
+++ Group16Alpha/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  Group16Alpha
+//
+//  Created by Wyllie, Ellen L on 10/25/17.
+//  Copyright © 2017 Wyllie, Ellen L. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signInButton.layer.cornerRadius = 10
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
---
+++ Group16Alpha/SignUpViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpViewController.swift
+//  Group16Alpha
+//
+//  Created by Wyllie, Ellen L on 10/25/17.
+//  Copyright © 2017 Wyllie, Ellen L. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        signUpButton.layer.cornerRadius = 10
+        
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
