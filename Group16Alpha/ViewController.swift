77f0e09cc2824bd1b6749bf103cff10b4fbd625a
Ellen Wyllie <elw2257@lib-pclem041.lib.utexas.edu>
2017-10-24 19:08:15 -0500

    Initial Commit

---
+++ Group16Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Group16Alpha
+//
+//  Created by Wyllie, Ellen L on 10/24/17.
+//  Copyright © 2017 Wyllie, Ellen L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
