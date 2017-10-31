011e607253f0c317ed463f6319d0d2aeb2e7c38c
Ryan Taylor <rmt926@lib-pclem031.lib.utexas.edu>
2017-10-30 21:14:25 -0500

    Available Pet List added and Pet Screen added Uses core data

---
+++ Group16Alpha/AvailablePetsTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  AvailablePetsTableViewController.swift
+//  Group16Alpha
+//
+//  Created by Taylor, Ryan M on 10/30/17.
+//  Copyright © 2017 Wyllie, Ellen L. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AvailablePetsTableViewController: UITableViewController {
+
+    var petList = [NSManagedObject]()
+    
+    func loadData()
+    {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Pet", in: managedContext)
+        
+        let pet = NSManagedObject(entity: entity!, insertInto:managedContext)
+        
+        pet.setValue(177, forKey: "age")
+        pet.setValue("Poodle", forKey: "breed")
+        pet.setValue("Male", forKey: "gender")
+        pet.setValue("Fido", forKey: "name")
+        pet.setValue("Large", forKey: "size")
+        pet.setValue("Dog", forKey: "type")
+        pet.setValue("Austin", forKey: "city")
+        pet.setValue("Texas", forKey: "state")
+        
+        
+        do {
+            try managedContext.save()
+        } catch {
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        petList.append(pet)
+    }
+    
+    override func viewDidLoad() {
+        print("Debug 1")
+        super.viewDidLoad()
+        print("Debug 2")
+        loadData()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return petList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "petCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
